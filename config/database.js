f95c4687f9e1aa97d0fe1faf4ca0e06589162f65
Ramnath K <ramnath2609@gmail.com>
2020-02-25 19:44:33 +0530

    changes made

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose= require('mongoose')
 require('dotenv').config()
 
 const setUpDB = () => {
-    mongoose.connect("mongodb+srv://ramanthdb:mz4258%46Y%42X]%35S?H@cluster0-mhiwr.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser : true, useUnifiedTopology : true }, () => {
+    mongoose.connect(process.env.DB_URI || "mongodb://localhost:27017/twitter-app", { useNewUrlParser : true, useUnifiedTopology : true }, () => {
         console.log('connected to db')
     })
 }
