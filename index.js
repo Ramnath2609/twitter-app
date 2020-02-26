b0ecedb23302734dfa24153de2edbf1ad05dad45
Ramnath K <ramnath2609@gmail.com>
2020-02-26 08:40:38 +0530

    message

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ app.use(cors())
 app.use(express.json())
 app.use('/', router)
 
-app.use(express.static(path.join(__dirname,"client/build"))) 
-app.get("*",(req,res) => { 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"client/build"))) 
+    app.get("*",(req,res) => { 
     res.sendFile(path.join(__dirname + "/client/build/index.html")) 
 })
+}
+
 
 
 app.listen(port, () => {
